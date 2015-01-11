e8ef4ee64d48af911714da48cf075fd7bd3997ea
TSadhu <tushar.sadhu08@gmail.com>
2015-01-11 16:35:08 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -3,3 +3,4 @@ source("load.R")
 png(filename = "plot1.png",width = 480, height = 480,units = "px")
 hist(Global_active_power,col = "red",main = "Global Active Power",
     xlab = "Global Active Power (kilowatts)",ylim = c(0, 1200))
+dev.off()    
