73ec327fb433a540946e39c5f79ddd1db487e786
TSadhu <tushar.sadhu08@gmail.com>
2015-01-11 16:46:50 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
 ##plot2
-source("load_data.R")
+source("load.R")
 png(filename = "plot2.png",width = 480,height = 480,units = "px")
 plot(DateTime, Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)")
 dev.off()
